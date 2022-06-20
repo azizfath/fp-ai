3422839b6d3c1c49372290afbdef97fd0906957e
renifatarina <reni.fatarina@gmail.com>
2022-06-20 08:22:52 +0700

    millis update

--- push/push-ai-esp8266.ino
+++ push/push-ai-esp8266.ino
@@ -1,8 +1,11 @@
 #include <ESP8266WiFi.h>
+
 #include <ESP8266HTTPClient.h>
 
 char ssid[] = "Kost Santri 3";
 char pass[] = "satusampailima";
+unsigned long timerDelay = 15000;
+unsigned long LastTime = 0;
 
 String api = "https://fp-ai.tukutahu.tk/insertData";
 HTTPClient http;
@@ -11,7 +14,7 @@ WiFiClientSecure client;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  WiFi.begin(ssid,pass);
+  WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
@@ -22,30 +25,31 @@ void setup() {
 }
 
 void loop() {
-  if(WiFi.status() == WL_CONNECTED){
+  if ((millis() - LastTime > timerDelay)) {
+    if (WiFi.status() == WL_CONNECTED) {
 
-        
-        http.begin(client,api.c_str());
-        http.addHeader("Content-Type", "application/json");
-//        String temperature = String(analogRead(temperaturePin));
-//        String humidity = String(analogRead(humidityPin));
-        String reqBody = "{\"deviceId\":\"esp8266\",\"tempereture\":69,\"humidity\":69}";
-        Serial.println("-----------------------------");
-        Serial.println(reqBody);
-        int httpResponseCode = http.POST(reqBody);
+      http.begin(client, api.c_str());
+      http.addHeader("Content-Type", "application/json");
+      //        String temperature = String(analogRead(temperaturePin));
+      //        String humidity = String(analogRead(humidityPin));
+      String reqBody = "{\"deviceId\":\"esp8266\",\"tempereture\":69,\"humidity\":69}";
+      Serial.println("-----------------------------");
+      Serial.println(reqBody);
+      int httpResponseCode = http.POST(reqBody);
 
-        if (httpResponseCode == 200) {
-            Serial.print("HTTP response code: ");
-            Serial.println(httpResponseCode);
-        }
-        else {
-            Serial.print("Error post data code: ");
-            Serial.println(httpResponseCode);
-            Serial.println(http.getString());
-        }
-        http.end();
-        delay(15000);
+      if (httpResponseCode == 200) {
+        Serial.print("HTTP response code: ");
+        Serial.println(httpResponseCode);
+      } else {
+        Serial.print("Error post data code: ");
+        Serial.println(httpResponseCode);
+        Serial.println(http.getString());
+      }
+      http.end();
+      delay(15000);
 
+    } else {
+      WiFi.reconnect();
     }
-
+  }
 }
